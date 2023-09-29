afdbfcd55c28fe25659b6fc4c24fc2829f9d57f9
giarsgo <giar@sgo.co.id>
2023-09-29 14:47:26 +0700

    update

--- resources/views/contents/home/partials/general.blade.php
+++ resources/views/contents/home/partials/general.blade.php
@@ -1,11 +1,14 @@
-<div class="row justify-content-center">
-  <select class="form-control form-control-sm select2" name="year2">
-    <option value="">-- Silakan Pilih --</option>
-    @foreach ($yearArr as $key => $value)
-    <option value="{{ Secure::secure($value['id']) }}" {{ date('Y') == $value['id'] ? 'selected' : ''
-      }}>{{ $value['name'] }}</option>
-    @endforeach
-  </select>
+<div class="form-group row justify-content-center mb-0">
+  <label class="col-sm-2 col-form-label">{{ __('Tahun Akademik') }}<code>*</code></label>
+  <div class="col-sm-2">
+    <select class="form-control form-control-sm select2" name="year2">
+      <option value="">-- Silakan Pilih --</option>
+      @foreach ($yearArr as $key => $value)
+      <option value="{{ Secure::secure($value['id']) }}" {{ date('Y') == $value['id'] ? 'selected' : ''
+        }}>{{ $value['name'] }}</option>
+      @endforeach
+    </select>
+  </div>
 </div>
 <div class="row justify-content-center">
   @foreach ($result as $value)
